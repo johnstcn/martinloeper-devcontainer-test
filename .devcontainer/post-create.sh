c9edb82f2b1e8adf345f8d5b5e87cb5c5cd69097
Martin Loeper <maddin.loeper@gmail.com>
2024-10-26 22:43:42 +0000

    fix: add vscode-server to PATH

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-echo "export PATH=/home/vscode/.asdf/shims:/home/vscode/.local/bin/:${NESTO_CONTAINER_WORKSPACE_ROOT}/scripts:$PATH" >> /home/vscode/.bashrc
+echo "export PATH=/tmp/code-server/bin:/home/vscode/.asdf/shims:/home/vscode/.local/bin/:${NESTO_CONTAINER_WORKSPACE_ROOT}/scripts:$PATH" >> /home/vscode/.bashrc
