5e7b068833f43e5e94804a6649d9112ad9bff329
Martin Löper <maddin.loeper@gmail.com>
2024-11-28 00:55:53 +0000

    fix: add coder binary to path

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-echo "export PATH=/tmp/code-server/bin:/root/.local/bin/:${NESTO_CONTAINER_WORKSPACE_ROOT}/scripts:$PATH" >> /root/.bashrc
+echo "export PATH=$(dirname $GIT_ASKPASS):/tmp/code-server/bin:/root/.local/bin/:${NESTO_CONTAINER_WORKSPACE_ROOT}/scripts:$PATH" >> /root/.bashrc
