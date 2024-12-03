3dcc6d560b33554fdb9f9bc3169b073d3bf9fbad
Martin Löper <maddin.loeper@gmail.com>
2024-12-03 23:49:17 +0000

    fix: post-create logic for coder bin dir determination

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-echo "export PATH=$(dirname $GIT_ASKPASS):/tmp/code-server/bin:/root/.local/bin/:${NESTO_CONTAINER_WORKSPACE_ROOT}/scripts:$PATH" >> /root/.bashrc
+echo "export CODER_EXECUTABLE_PATH=$(find /tmp -name 'coder.*'); export PATH=\$CODER_EXECUTABLE_PATH:/tmp/code-server/bin:/root/.local/bin/:${NESTO_CONTAINER_WORKSPACE_ROOT}/scripts:$PATH" >> /root/.bashrc
